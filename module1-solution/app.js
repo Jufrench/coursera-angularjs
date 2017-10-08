d6a4b7b0a5f4ca0555d31df7ab1e641165be274a
Julian French <ju.french@gmail.com>
2017-10-07 19:42:51 -0500

    Basics complete

---
+++ module1-solution/app.js
@@ -0,0 +1,34 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.lunches = "";
+
+  $scope.checkIfTooMuch = () => {
+    var lunchAmount = $scope.lunches;
+    var res = lunchAmount.split(",");
+    if (res.length < 4 && lunchAmount !== "") {
+      $scope.display = "Enjoy!";
+    } else if (lunchAmount === "" && lunchAmount.length === 0) {
+      $scope.display = "Please enter data first";
+      // I do NOT consider an empty item, i.e., , , as an item towards to the count.
+    } else {
+      $scope.display = "Too Much!";
+    }
+  };
+
+  // $scope.redBorder = () => {
+  //   if ($scope.display === "Please enter data first") {
+  //     {'border-color': 'red'};
+  //   }
+  // }
+}
+
+
+
+
+})();
